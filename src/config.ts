93b812e34a628df789fc125033b660fe9155a2f8
Moksha <mokshyagnay@gmail.com>
2026-04-16 19:17:48 +0530

    first commit

---
+++ src/config.ts
@@ -0,0 +1,78 @@
+export const siteConfig = {
+  name: "Jaya Naga Sai Jithendra Mamidi",
+  title: "Student",
+  description: "Portfolio website of Jaya Naga Sai Jithendra",
+  accentColor: "#0037ff",
+  social: {
+    email: "jithendramamidi116@gmail.com",
+    linkedin: "https://linkedin.com/in/jaya-naga-sai-jithendra-mamidi-73380232b",
+    github: "https://github.com/Jithendra-01",
+  },
+  aboutMe:
+    "Passionate software developer with a strong foundation in full-stack development. Experienced in building scalable web applications using modern technologies. Eager to learn and grow in a collaborative environment.",
+  skills: [ "React", "Python", "HTML", "SQL", "Java"],
+  projects: [
+    {
+      name: "Attendance Management",
+      description:
+        "A fullStack android application which is used to take attendance across the campus for multiple faculty as well as a HOD dashboard for monitoring and Analytics",
+      link: "#",
+      skills: ["React", "Node.js", "Mongodb"],
+    },
+    {
+      name: "EventHub",
+      description:
+        "Full-Stack event management application enabling student registration and organizer-led event, attendance and analytics management",
+      link: "#",
+      skills: ["React", "Node.js", "Mongodb"],
+    },
+    {
+      name: "To-do list",
+      description:
+        "A web based task management system that helps users to organise and track their daily activities .",
+      link: "#",
+      skills: ["React", "Node.js"],
+    },
+  ],
+  //experience: [
+  //  {
+  //    company: "Tech Company",
+  //    title: "Senior Software Engineer",
+  //    dateRange: "Jan 2022 - Present",
+  //    bullets: [
+  //      "Led development of microservices architecture serving 1M+ users",
+  //      "Reduced API response times by 40% through optimization",
+  //      "Mentored team of 5 junior developers",
+  //    ],
+  //  },
+  //],
+  education: [
+    {
+      school: "DVR & Dr.Hs MIC College ot Technology",
+      degree: "Bachelor of Technology in Computer Science and Engineering",
+      dateRange: "2023 - 2027 (ongoing)",
+      achievements: [
+        "I am our Section representative managing all student related activites within our class",
+        "Graduated with honors with zero backlogs",
+        "Specialized in React and Node.js",
+      ],
+    },
+    {
+      school: "KBN Shine (Board of Intermediate)",
+      degree: "Intermediate",
+      dateRange: "2021-2023",
+      achievements: [
+        "Graduated with honors with zero-backlogs",
+        "",
+      ],
+    },
+    {
+      school: "Ravindra Bharathi High School",
+      degree: "High School",
+      dateRange: "2020-2021",
+      achievements: [
+        "Graduated with honors having 100%",
+      ],
+    },
+  ],
+};
